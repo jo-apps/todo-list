08fb9775a3ef1458290591fac0d6ddf102f6fa6e
jo-apps <92635761+jo-apps@users.noreply.github.com>
2022-07-31 16:16:33 +0200

    add toggle checkmark on table cells

--- Todo List/Todo List/TodoListViewController.swift
+++ Todo List/Todo List/TodoListViewController.swift
@@ -23,6 +23,13 @@ class TodoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+       if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark  {
+           tableView.cellForRow(at: indexPath)?.accessoryType = .none
+       }
+       else {
+           tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+       }
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
